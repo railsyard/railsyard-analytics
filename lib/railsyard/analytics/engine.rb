0152b3b65adf67b935d2d62ebfa7e72216c3fa97
Stefano Verna <stefano.verna@welaika.com>
2012-06-06 17:55:35 +0200

    Added precompilation asset

--- lib/railsyard/analytics/engine.rb
+++ lib/railsyard/analytics/engine.rb
@@ -2,7 +2,13 @@ module Railsyard
   module Analytics
     class Engine < ::Rails::Engine
       config.to_prepare do
-        puts "PREPARED"
+
+        initializer "Railsyard precompile hook", :group => :assets do |app|
+          app.config.assets.precompile += [
+            "railsyard/analytics.js"
+          ]
+        end
+
         Railsyard::Backend.plugin_manager.add_plugin(:railsyard_analytics) do
           name "Railsyard Analytics"
           backend_javascript_dependency "https://www.google.com/jsapi"
@@ -10,6 +16,7 @@ module Railsyard
           dashboard_widget :analytics_line_chart, cell: :railsyard_analytics, action: :line_chart
           dashboard_widget :analytics_pie_chart, cell: :railsyard_analytics, action: :pie_chart
         end
+
       end
     end
   end
